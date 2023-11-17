1160a6d2138d8c598c158efcc5ede6afc586d65c
mhernangilp <mhernangilp@gmail.com>
2023-11-17 14:30:22 +0100

    cd done

--- src/execution/cd.c
+++ src/execution/cd.c
@@ -1,24 +1,54 @@
 #include "../../minishell.h"
+#include <dirent.h>
 #include <linux/limits.h>
+#include <stdio.h>
 
-static	char	*get_pwd();
+static char	*get_pwd();
+static int	chpwd();
 
 int	cd(char **commands)
 {
 	char	*chdirectory;
-	char	*pwd;
-	char	*pwd_env;
-	int		error;
+	DIR	*dir;
 
 	if (commands[1] && commands[2])
+	{
+		ft_putstr_fd("too many arguments\n", 2);
 		return (1);
+	}
 	if (!commands[1])
-		chdirectory = getenv("HOME"); // comprobar si home existe
+	{
+		chdirectory = getenv_value("HOME");
+		if (!chdirectory)
+		{
+			ft_putstr_fd("HOME not set\n", 2);
+			return (1);
+		}
+	}
 	else
 		chdirectory = commands[1];
-	if (*chdirectory == '\0')
+	dir = opendir(chdirectory);
+	if (!dir)
+	{
+		perror(chdirectory);
 		return (1);
-	error = chdir(chdirectory);
+	}
+	closedir(dir);
+	if (chdir(chdirectory) == -1)
+	{
+		perror(chdirectory);
+		return (1);
+	}
+	if (chpwd())
+		return (1);
+	return (0);
+}
+
+static int	chpwd()
+{
+	char	*pwd;
+	char	*pwd_env;
+
 	pwd = get_pwd();
 	if (!pwd)
 		return (1);
@@ -28,7 +58,7 @@ int	cd(char **commands)
 	add(pwd_env);
 	free(pwd);
 	free(pwd_env);
-	return (error);
+	return (0);
 }
 
 static char	*get_pwd()
