bd83d0c7f49c574eeedb21595bd02b56f20af990
Marcos Hernangil Prats <mhernang@c3r3s2.42madrid.com>
2023-12-21 18:43:18 +0100

    fixed cd ~

--- src/execution/cd.c
+++ src/execution/cd.c
@@ -6,14 +6,14 @@
 /*   By: mhernang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 18:36:21 by mhernang          #+#    #+#             */
-/*   Updated: 2023/11/30 12:48:13 by mhernang         ###   ########.fr       */
+/*   Updated: 2023/12/21 18:13:17 by mhernang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 static char	*set_chdir(t_bridge *bridge, char **commands);
-static int	check_dir(char *chdirectory);
+static int	check_dir(char *chdirectory, char *arg);
 static char	*get_pwd(void);
 static int	chpwd(t_bridge *bridge);
 
@@ -31,17 +31,32 @@ int	cd(t_bridge *bridge, char **commands)
 	chdirectory = set_chdir(bridge, commands);
 	if (!chdirectory)
 		return (1);
-	if (check_dir(chdirectory))
+	if (check_dir(chdirectory, commands[1]))
 		return (1);
 	if (chdir(chdirectory) == -1)
 	{
 		ft_putstr_fd("minishell: cd: ", 2);
 		perror(chdirectory);
+		free(chdirectory);
 		return (1);
 	}
+	free(chdirectory);
 	if (chpwd(bridge))
 		return (1);
 	return (0);
+}
+
+static char	*parse_chdir(char *arg, t_bridge *bridge)
+{
+	char	*ret;
+
+	if (arg[0] == '~')
+	{
+		ret = ft_strjoin(getenv_value(bridge -> m_env, "HOME"), &arg[1]);
+	}
+	else
+		ret = ft_strdup(arg);
+	return (ret);
 }
 
 static char	*set_chdir(t_bridge *bridge, char **commands)
@@ -50,7 +65,7 @@ static char	*set_chdir(t_bridge *bridge, char **commands)
 
 	if (!commands[1])
 	{
-		chdirectory = getenv_value(bridge -> m_env, "HOME");
+		chdirectory = ft_strdup(getenv_value(bridge -> m_env, "HOME"));
 		if (!chdirectory)
 		{
 			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
@@ -58,11 +73,11 @@ static char	*set_chdir(t_bridge *bridge, char **commands)
 		}
 	}
 	else
-		chdirectory = commands[1];
+		chdirectory = parse_chdir(commands[1], bridge);
 	return (chdirectory);
 }
 
-static int	check_dir(char *chdirectory)
+static int	check_dir(char *chdirectory, char *arg)
 {
 	DIR		*dir;
 
@@ -70,7 +85,11 @@ static int	check_dir(char *chdirectory)
 	if (!dir)
 	{
 		ft_putstr_fd("minishell: cd: ", 2);
-		perror(chdirectory);
+		if (arg[1] == '~')
+			perror(arg);
+		else
+			perror(chdirectory);
+		free(chdirectory);
 		return (1);
 	}
 	closedir(dir);
